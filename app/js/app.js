b1bbaddc0b8711f06d0d8d4dba3f05a9be69bca4
tbauer516 <tcbauer516@gmail.com>
2017-02-10 23:42:00 -0800

    added artyom and removed all other keyword recognition libraries

--- app/js/app.js
+++ app/js/app.js
@@ -42,7 +42,7 @@ var renderCurrentData  = function(data) {
 	let rainE = $('<p class="block">' + data.precip + '%</p>');
 
 	// head.append(divTemplate.clone().addClass('weather-main-time').append(timeE);
-	$('.weather-main-icon').empty().load('../assets/weather/' + data.icon + '.svg');
+	$('.weather-main-icon').empty().load('/app/assets/weather/' + data.icon + '.svg');
 	$('.weather-main-stats').empty().append(tempE).append(windE).append(rainE);
 }
 
@@ -69,7 +69,7 @@ var renderForecastData = function(data) {
 		// let iconE = fs.readFileSync('app/assets/weather/' + data[i].icon + '.svg', 'utf8');
 		// iconE = $(iconE);
 
-		weather.append(divTemplate.clone().load('../assets/weather/' + data[i].icon + '.svg'));
+		weather.append(divTemplate.clone().load('/app/assets/weather/' + data[i].icon + '.svg'));
 		weather.append(divTemplate.clone().append(dayE));
 		weather.append(divTemplate.clone().append(highE).append(lowE));
 		weather.append(divTemplate.clone().append(windE).append(rainE));
@@ -196,6 +196,7 @@ var closeAll = function() {
 
 var openWeather = function() {
 	closeAll();
+	displayOn = true;
 	weatherDisplay.removeClass('hidden');
 	weatherDisplay.addClass('visible');
 }
@@ -207,71 +208,6 @@ setInterval(renderTime, 1000);
 setInterval(getCurrentData, 300000);
 
 setInterval(getForecastData, 21600000); // Refresh every 6 hours
-
-
-let startRecognition = function() {
-	recognition = new webkitSpeechRecognition();
-	// recognition.grammars = speechRecognitionList;
-	// recognition.continuous = true;
-
-	recognition.onstart = function(event) {
-		respond('I\'m listening...');
-		console.log('recording');
-	};
-	recognition.onresult = function(event) {
-		recognition.onend = null;
-
-		var text = "";
-		for (var i = event.resultIndex; i < event.results.length; ++i) {
-			text += event.results[i][0].transcript;
-		}
-		respond('You said: ' + text);
-		setTimeout(function() {
-			respond('I\'m listening...');
-		}, 3000);
-		stopRecognition();
-		console.log(text);
-		if (text.MC('what is weather')) {
-			let weather = $('.weather');
-			weather.removeClass('hidden');
-			weather.addClass('visible');
-		} else if (text.MC('(close clothes) weather')) {
-			let weather = $('.weather');
-			weather.removeClass('visible');
-			weather.addClass('hidden');
-		}
-	};
-	recognition.onend = function() {
-		// respond('I couldn\'t hear you. Can you repeat that?');
-		stopRecognition();
-		console.log('end');
-	};
-	recognition.onerror = function(error) {
-		console.log(error);
-	};
-	recognition.lang = "en-US";
-	recognition.start();
-}
-
-let stopRecognition = function() {
-	if (recognition) {
-		recognition.stop();
-		recognition = null;
-		startRecognition();
-	}
-}
-
-let switchRecognition = function() {
-	if (recognition) {
-		stopRecognition();
-	} else {
-		startRecognition();
-	}
-}
-
-let respond = function(message) {
-	$('.communication').empty().append($('<p></p>').text(message));
-}
 
 // let recognition;
 // startRecognition();
@@ -328,260 +264,18 @@ openWeather();
 
 // ##############################################
 
+console.log(artyom);
 
-// These will be initialized later
-var recognizer, recorder, callbackManager, audioContext, outputContainer;
-// Only when both recorder and recognizer do we have a ready application
-var isRecorderReady = isRecognizerReady = false;
+artyom.on(["Weather Display", "Whether Display"]).then(function(i){
+    console.log("Triggered");
+	toggleDisplay();
+});
 
-// A convenience function to post a message to the recognizer and associate
-// a callback to its response
-function postRecognizerJob(message, callback) {
-	var msg = message || {};
-	if (callbackManager) msg.callbackId = callbackManager.add(callback);
-	if (recognizer) recognizer.postMessage(msg);
-};
-
-// This function initializes an instance of the recorder
-// it posts a message right away and calls onReady when it
-// is ready so that onmessage can be properly set
-function spawnWorker(workerURL, onReady) {
-	recognizer = new Worker(workerURL);
-	recognizer.onmessage = function(event) {
-		onReady(recognizer);
-	};
-	recognizer.postMessage('');
-};
-
-// To display the hypothesis sent by the recognizer
-function updateCount(count) {
-	if (outputContainer) outputContainer.innerHTML = count;
-};
-
-// This updates the UI when the app might get ready
-// Only when both recorder and recognizer are ready do we enable the buttons
-function updateUI() {
-	if (isRecorderReady && isRecognizerReady) startBtn.disabled = false;
-	// TODO: uncomment and add to if statement in production
-	//startRecording();
-};
-
-// This is just a logging window where we display the status
-function updateStatus(newStatus) {
-	document.getElementsByClassName('current-status')[0].innerHTML += '<br/>' + newStatus;
-};
-
-// A not-so-great recording indicator
-function displayRecording(display) {
-	if (display) document.getElementById('recording-indicator').innerHTML = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
-	else document.getElementById('recording-indicator').innerHTML = '';
-};
-
-// Callback function once the user authorises access to the microphone
-// in it, we instanciate the recorder
-function startUserMedia(stream) {
-	var input = audioContext.createMediaStreamSource(stream);
-	// Firefox hack https://support.mozilla.org/en-US/questions/984179
-	window.firefox_audio_hack = input;
-	var audioRecorderConfig = {errorCallback: function(x) {updateStatus('Error from recorder: ' + x);}};
-	recorder = new AudioRecorder(input, audioRecorderConfig);
-	// If a recognizer is ready, we pass it to the recorder
-	if (recognizer) recorder.consumers = [recognizer];
-	isRecorderReady = true;
-	updateUI();
-	updateStatus('Audio recorder ready');
-};
-
-// This starts recording. We first need to get the id of the keyword search to use
-var startRecording = function() {
-	var id = document.getElementById('keyword').value;
-	if (recorder && recorder.start(id)) displayRecording(true);
-	stopBtn.disabled = false;
-	startBtn.disabled = true;
-	console.log('Listening...');
-};
-
-// Stops recording
-var stopRecording = function() {
-	recorder && recorder.stop();
-	displayRecording(false);
-	stopBtn.disabled = true;
-	startBtn.disabled = false;
-};
-
-// Called once the recognizer is ready
-// We then add the grammars to the input select tag and update the UI
-var recognizerReady = function() {
-	// updateKeywords();
-	isRecognizerReady = true;
-	updateUI();
-	updateStatus('Recognizer ready');
-};
-
-// We get the grammars defined below and fill in the input select tag
-// var updateKeywords = function() {
-// 	var selectTag = document.getElementById('keyword');
-// 	for (var i = 0 ; i < keywordIds.length ; i++) {
-// 		var newElt = document.createElement('option');
-// 		newElt.value=keywordIds[i].id;
-// 		newElt.innerHTML = keywordIds[i].title;
-// 		selectTag.appendChild(newElt);
-// 	}
-// };
-
-// This adds a keyword search from the array
-// We add them one by one and call it again as
-// a callback.
-// Once we are done adding all grammars, we can call
-// recognizerReady()
-var feedKeyword = function(g, index, id) {
-	if (id && (keywordIds.length > 0)) keywordIds[0].id = id.id;
-	if (index < g.length) {
-		keywordIds.unshift({title: g[index].title})
-		postRecognizerJob({command: 'addKeyword', data: g[index].g},
-				function(id) {feedKeyword(keywords, index + 1, {id:id});});
-	} else {
-		recognizerReady();
-	}
-};
-
-// This adds words to the recognizer. When it calls back, we add grammars
-var feedWords = function(words) {
-	postRecognizerJob({command: 'addWords', data: words},
-			function() {feedKeyword(keywords, 0);});
-};
-
-// This initializes the recognizer. When it calls back, we add words
-var initRecognizer2 = function() {
-	// You can pass parameters to the recognizer, such as : {command: 'initialize', data: [['-hmm', 'my_model'], ['-fwdflat', 'no']]}
-	postRecognizerJob({command: 'initialize', data: [['-kws_threshold', '1e-35'], ['-kws', 'keyphrase.list'], ['-dict', 'keyphrase.dict']]},
-			function() {
-				if (recorder) recorder.consumers = [recognizer];
-				// feedWords(wordList);
-			}
-	);
-	postRecognizerJob({command: 'load', callbackId: 0, data: ['keyphrase-list.js']})
-};
-
-function initRecognizer3() {
-	recognizer.postMessage({
-		command: 'lazyLoad',
-		callbackId: callbackManager.add(
-			function() {
-				recognizerReady();
-
-				// We pass dictionary and keyword parameters to the recognizer
-				postRecognizerJob({
-					command: 'initialize',
-					data: [
-						['-kws_threshold', '1e-35'],
-						['-kws', 'keyphrase.list'],
-						['-dict', 'keyphrase.dict']
-					]
-				}, function() {
-					// startRecording();
-					updateUI();
-				});
-
-			}
-		),
-		data: {
-			folders: [['/', 'modelFiles']],
-			files: [['/modelFiles', 'keyphrase.list', '/../lib/keyphrase.list'],
-					['/modelFiles', 'keyphrase.dict', '/../lib/keyphrase.dict']]
-		}
-	});
-}
-
-var initRecognizer = function() {
-	postRecognizerJob({
-		command: 'load',
-		data: ['load-data.js'] //this path is relative to the web-worker!
-	},
-	function() {
-		postRecognizerJob({
-			command: 'initialize',
-			data:[ //these paths are the emscripten virtual paths you setup in load-data.js
-				['-kws_threshold', '1e-35'],
-				['-kws', 'keyphrase.list'],
-				['-dict', 'keyphrase.dict']
-			]
-		},
-		function() {
-			if (recorder) recorder.consumers = [recognizer];
-			recognizerReady();
-		});
-	});
-}
-
-// When the page is loaded, we spawn a new recognizer worker and call getUserMedia to
-// request access to the microphone
-window.onload = function() {
-	outputContainer = document.getElementsByClassName('output')[0];
-	updateStatus('Initializing web audio and speech recognizer, waiting for approval to access the microphone');
-	callbackManager = new CallbackManager();
-	spawnWorker('js/recognizer.js', function(worker) {
-		// This is the onmessage function, once the worker is fully loaded
-		worker.onmessage = function(e) {
-			// This is the case when we have a callback id to be called
-			if (e.data.hasOwnProperty('id')) {
-				var clb = callbackManager.get(e.data['id']);
-				var data = {};
-				if ( e.data.hasOwnProperty('data')) data = e.data.data;
-				if(clb) clb(data);
-			}
-			// This is a case when the recognizer has a new count number
-			if (e.data.hasOwnProperty('hyp')) {
-				var newCount = e.data.hyp;
-				// TODO: logic for recognition here
-				if (e.data.hasOwnProperty('final') &&  e.data.final) {
-					newCount = 'Final: ' + newCount;
-					console.log(e.data.hyp.trim());
-					if (e.data.hyp.trim() == 'MAGIC CLOSE')
-						closeAll();
-					else if (e.data.hyp.trim() == 'MAGIC WEATHER')
-						openWeather();
-				}
-				if (e.data.hyp != '') {
-					updateCount(newCount);
-					stopRecording();
-					startRecording();
-				}
-			}
-			// This is the case when we have an error
-			if (e.data.hasOwnProperty('status') && (e.data.status == 'error')) {
-				updateStatus('Error in ' + e.data.command + ' with code ' + e.data.code);
-			}
-		};
-		// Once the worker is fully loaded, we can call the initialize function
-		initRecognizer();
-	});
-
-	// The following is to initialize Web Audio
-	try {
-		window.AudioContext = window.AudioContext || window.webkitAudioContext;
-		navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-		window.URL = window.URL || window.webkitURL;
-		audioContext = new AudioContext();
-	} catch (e) {
-		updateStatus('Error initializing Web Audio browser');
-	}
-	if (navigator.getUserMedia) navigator.getUserMedia({audio: true}, startUserMedia, function(e) {
-		updateStatus('No live audio input in this browser');
-	});
-	else updateStatus('No web audio support in this browser');
-
-	// Wiring JavaScript to the UI
-	var startBtn = document.getElementById('startBtn');
-	var stopBtn = document.getElementById('stopBtn');
-	startBtn.disabled = true;
-	stopBtn.disabled = true;
-	startBtn.addEventListener('click', startRecording);
-	stopBtn.addEventListener('click', stopRecording);
-};
-
-// This is the list of words that need to be added to the recognizer
-// This follows the CMU dictionary format
-// var wordList = [['MAGIC', 'M AE JH IH K'], ['WHETHER', 'W EH DH ER'], ['WEATHER', 'W EH DH ER'], ['CLOSE', 'K L OW ZH']];
-// var keywords = [{title: 'Open Weather', g: 'MAGIC WHETHER'}, {title: 'Close All', g: 'MAGIC CLOSE'}, {title: 'Open Weather', g: 'MAGIC WEATHER'}];
-var keywordIds = [];
+artyom.initialize({
+    lang:"en-US",
+	continuous: true,
+    debug:true, // Show what recognizes in the Console
+    listen:true, // Start listening after this
+    speed:0.9, // Talk a little bit slow
+    mode:"normal" // This parameter is not required as it will be normal by default
+});
--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const compression = require('compression');
 const app = express();
+const fs = require('fs');
 
 const oneDay = 86400000;
 
-const fs = require('fs');
+const port = 8108;
 
-const API_KEY = fs.readFileSync('app/js/darknet.txt', 'utf8');
+const API_KEY = fs.readFileSync('app/resources/darknet.txt', 'utf8');
 
 app.use(compression());
 
-app.use('/', express.static(__dirname + '/app', {maxAge: oneDay}));
+app.use('/', express.static(__dirname /*+ '/app'*/, {maxAge: oneDay}));
 
 app.get('/API', function(req, res, next) {
     res.send(API_KEY);
@@ -22,9 +23,7 @@ app.get('/ICON', function(req, res, next) {
     res.send(icon);
 });
 
-app.get('/')
-
-var port = 8108;
+app.get('/');
 
 app.listen(port, function() {
   console.log(`Application worker ${process.pid} started...`);
